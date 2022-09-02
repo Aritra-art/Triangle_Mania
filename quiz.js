2ef8d4988c07117c19d3c56ad7b1d248d6dca90e
aritra <aritrachowdhury65@gmail.com>
2022-09-03 00:48:14 +0530

    changed

--- quiz.js
+++ quiz.js
@@ -1,34 +1,34 @@
 var quizForm = document.querySelector(".quiz-form");
-  var check = document.querySelector(".submit");
-  var clear = document.querySelector(".clear")
-  var show = document.querySelector(".show");
+var check = document.querySelector(".submit");
+var clear = document.querySelector(".clear");
+var show = document.querySelector(".show");
 
-  var correctAnswers = [
-    "90°",
-    "right-angled",
-    "one right angle",
-    "12, 16, 20",
-    "Equilateral Triangle",
-    "100°",
-    "30°"
-  ]
+var correctAnswers = [
+  "90°",
+  "right-angled",
+  "one right angle",
+  "12, 16, 20",
+  "Equilateral Triangle",
+  "100°",
+  "30°",
+];
 
-  clear.addEventListener("click", clearEventHandler);
-  check.addEventListener("click", checkEventHandler);
+clear.addEventListener("click", clearEventHandler);
+check.addEventListener("click", checkEventHandler);
 
-  function clearEventHandler(){
-    window.location.reload();
-  }
+function clearEventHandler() {
+  show.textContent = "";
+}
 
-  function checkEventHandler(){
-    var index = 0;
-    var score = 0;
-    var formResults = new FormData(quizForm);
-    for(var value of formResults.values()){
-      if(value === correctAnswers[index]){
-        score ++;
-      }
-      index++;
+function checkEventHandler() {
+  var index = 0;
+  var score = 0;
+  var formResults = new FormData(quizForm);
+  for (var value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score++;
     }
-    show.innerText = `Your Score is ${score}`
+    index++;
   }
+  show.innerText = `Your Score is ${score}`;
+}
