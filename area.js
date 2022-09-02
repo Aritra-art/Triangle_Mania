07a562b3a713abef53745f25c38c1b135e042b73
aritra <aritrachowdhury65@gmail.com>
2022-09-02 13:48:31 +0530

    area negative handled

--- area.js
+++ area.js
@@ -8,19 +8,29 @@ var show = document.querySelector(".show");
 clear.addEventListener("click", clearEventHandler);
 check.addEventListener("click", checkEventHandler);
 
-function clearEventHandler(){
-  window.location.reload();
+function clearEventHandler() {
+  base.value = "";
+  height.value = "";
+  show.style.display = "none";
+  error.style.display = "none";
 }
 
-function checkEventHandler(){
-  if(base.value!= "" && height.value!= ""){
-    error.style.display = "none";
-    show.style.display = "block";
-    var area = 0.50 * (Number(base.value) * Number(height.value));
-    show.innerText = `The Area of a triangle whose base is ${base.value} and height is ${height.value} is ${area} square units`
-  }else{
+function checkEventHandler() {
+  if (base.value != "" && height.value != "") {
+    if (Number(base.value) > 0 && Number(height.value) > 0) {
+      error.style.display = "none";
+      show.style.display = "block";
+      var area = 0.5 * (Number(base.value) * Number(height.value));
+      show.innerText = `The Area of a triangle whose base is ${base.value} and height is ${height.value} is ${area} square units`;
+    } else {
+      error.style.display = "block";
+      show.style.display = "none";
+      error.textContent =
+        "The Base and Height both should be Positive of a Triangle";
+    }
+  } else {
     error.style.display = "block";
     show.style.display = "none";
-    error.innerText = "Please enter both the values"
+    error.innerText = "Please enter both the values";
   }
 }
